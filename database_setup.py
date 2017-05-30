b29436c4c18b7cd85e67583b0fcd67e9875915e9
Kurt Anderson <Kurtanderson234@gmail.com>
2017-05-30 10:02:25 -0500

    Created Database Structure

--- database_setup.py
+++ database_setup.py
@@ -6,13 +6,11 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Item(Base):
-    __tablename__ = 'items'
+class Category(Base):
+    __tablename__ = 'category'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    description = Column(String(250))
-    category = Column(String(250), nullable=False)
 
     @property
     def serialize(self):
@@ -20,32 +18,26 @@ class Item(Base):
         return {
             'name': self.name,
             'id': self.id,
-            'description': self.description,
-            'category': self.category
         }
 
 
-# class MenuItem(Base):
-#     __tablename__ = 'menu_item'
-#
-#     name = Column(String(80), nullable = False)
-#     id = Column(Integer, primary_key = True)
-#     description = Column(String(250))
-#     price = Column(String(8))
-#     course = Column(String(250))
-#     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
-#     restaurant = relationship(Restaurant)
-#
-#     @property
-#     def serialize(self):
-#         """Return object data in easily serializeable format"""
-#         return {
-#            'name': self.name,
-#            'description': self.description,
-#            'id': self.id,
-#            'price': self.price,
-#            'course': self.course,
-#         }
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+           'name': self.name,
+           'description': self.description,
+           'id': self.id,
+        }
 
 engine = create_engine('sqlite:///restaurantmenu.db')
  
