fd73b600dcb3b73dc14652f1523a1ea8c5cbe1d2
Kurt Anderson <Kurtanderson234@gmail.com>
2017-06-05 16:43:13 -0500

    Added Local Permission System

--- database_setup.py
+++ database_setup.py
@@ -20,6 +20,8 @@ class Category(Base):
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -38,6 +40,8 @@ class CategoryItem(Base):
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
